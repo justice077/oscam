550833db2257f18f3229b0ded5aeb619f48f9f62
nx111 <gd.zhangdz@gmail.com>
2016-08-08 21:17:40 +0800

    add module emulator from github.com/oscam-emu

--- readers.h
+++ readers.h
@@ -17,5 +17,5 @@ extern const struct s_cardsystem reader_bulcrypt;
 extern const struct s_cardsystem reader_griffin;
 extern const struct s_cardsystem reader_dgcrypt;
 extern const struct s_cardsystem reader_streamguard;
-
+extern const struct s_cardsystem reader_emu;
 #endif
