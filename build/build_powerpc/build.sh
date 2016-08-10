4285b9acb680647a81ba47b5128b01d675f523b9
nx111 <gd.zhangdz@gmail.com>
2016-08-10 13:02:53 +0800

    can run build scripts from elsewhere.

--- build/build_powerpc/build.sh
+++ build/build_powerpc/build.sh
@@ -2,18 +2,48 @@
 plat=powerpc
 plat_dir=build_powerpc
 TOOLCHAIN=powerpc-tuxbox-linux-gnu
+#TOOLCHAINROOT=/work/dreambox/toolchains
 
+##############################################################
 curdir=`pwd`
-builddir=$(cd $(dirname $0);pwd)
-ROOT=`pwd`
+builddir=`cd $(dirname $0);pwd`
+
+if [ "${OSCAM_SRC}" != "" -a -f ${OSCAM_SRC}/oscam.c ]; then
+	ROOT=${OSCAM_SRC}
+elif [ -f $(dirname $(dirname $builddir))/oscam.c ]; then
+	ROOT=$(dirname $(dirname $builddir))
+else
+	echo "Not found oscam source directory! Please set OSCAM_SRC environment value..."
+	cd $curdir
+	exit
+fi
+
+# fix config.sh for subverison changed to git
+if [ -f $ROOT/config.sh ]; then
+	cp $ROOT/config.sh $ROOT/config.sh.orig
+	sed -e "s/^[[:space:]]*(svnversion .*/\
+		revision=\`(svnversion -n . 2>\/dev\/null || printf 0) | sed \'s\/.*:\/\/; s\/[^0-9]*$\/\/; s\/^$\/0\/'\`\n\
+\n\
+		if [ \"\$revision\" = \"\" -o \"\$revision\" = \"0\" ]; then\n\
+			git log  | grep git-svn-id | sed -n 1p | cut -d@ -f2 | cut -d' ' -f1\n\
+		else\n\
+			echo \$revision\n\
+		fi\n\
+/" -i $ROOT/config.sh
+fi
 
 [ -d $ROOT/build/.tmp ] || mkdir -p $ROOT/build/.tmp
 rm -rf $ROOT/build/.tmp/*
 
-TOOLCHAINROOT=$(dirname $ROOT)/toolchains
-
+[ "$TOOLCHAINROOT" = "" ] && TOOLCHAINROOT=$(dirname $ROOT)/toolchains
+if [ ! -f $TOOLCHAINROOT/$TOOLCHAIN/bin/$TOOLCHAIN-gcc ]; then
+	echo "Not found $TOOLCHAIN-gcc..."
+	exit -1
+fi
 ##################################################################
 cd $ROOT/build/.tmp
+cp $ROOT/config.h $ROOT/config.h.orig
+
 PATH=$TOOLCHAINROOT/$TOOLCHAIN/bin:$PATH \
    cmake  -DCMAKE_TOOLCHAIN_FILE=$ROOT/toolchains/toolchain-powerpc-tuxbox.cmake\
 	  --clean-first -DWEBIF=1 \
@@ -25,21 +55,23 @@ PATH=$TOOLCHAINROOT/$TOOLCHAIN/bin:$PATH \
 	  $ROOT
 
 make
+[ -f $ROOT/config.h.orig ] && mv $ROOT/config.h.orig $ROOT/config.h
 
-[ -d $ROOT/build/${plat_dir}/image/var/bin ] || mkdir -p $ROOT/build/${plat_dir}/image/var/bin
-cp $ROOT/build/.tmp/oscam $ROOT/build/${plat_dir}/image/var/bin/
+[ -d ${builddir}/image/var/bin ] || mkdir -p ${builddir}/image/var/bin
+cp $ROOT/build/.tmp/oscam ${builddir}/image/var/bin/
 
 ##################################################################
 
 svnver=`$ROOT/config.sh --oscam-revision`
+[ -f $ROOT/config.sh.orig ] && mv $ROOT/config.sh.orig $ROOT/config.sh
 
-cd $ROOT/build/${plat_dir}/image
+cd ${builddir}/image
 if [ $# -ge 1 -a "$1" = "-debug" ]; then
 	compile_time=$(date +%Y%m%d%H%M)D
 else
 	compile_time=$(date +%Y%m%d)
 fi
-tar czf $ROOT/build/oscam-${plat}-r${svnver}-nx111-${compile_time}.tar.gz *
+tar czf $(dirname $builddir)/oscam-${plat}-r${svnver}-nx111-${compile_time}.tar.gz *
 
 rm -rf $ROOT/build/.tmp/*
 cd $curdir
