2c34e94720e755f70de49fc56fcd8bbde7d3564e
nx111 <gd.zhangdz@gmail.com>
2016-08-10 13:03:34 +0800

    add build all script

---
+++ build/build_all.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+curdir=`pwd`
+builddir=`cd $(dirname $0);pwd`
+
+if [ "${OSCAM_SRC}" != "" -a -f ${OSCAM_SRC}/oscam.c ]; then
+	ROOT=${OSCAM_SRC}
+elif [ -f $(dirname $(dirname $builddir))/oscam.c ]; then
+	ROOT=$(dirname $(dirname $builddir))
+else
+	echo "Not found oscam source directory! Please set OSCAM_SRC environment value..."
+	cd $curdir
+	exit
+fi
+
+find $builddir -name build.sh | while read f; do
+	OSCAM_SRC=$ROOT $f
+done
+
+cd $curdir
